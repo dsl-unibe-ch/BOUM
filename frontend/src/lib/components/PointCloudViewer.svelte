2bfeb67ae45c215bd0c2cd60da1d70ad9840fce1
flicksolutions <sebastian@flick.solutions>
2026-09-17 21:28:57 +0200

    feat: add ways to enlarge the pointcloud

--- frontend/src/lib/components/PointCloudViewer.svelte
+++ frontend/src/lib/components/PointCloudViewer.svelte
@@ -1,11 +1,48 @@
 <script lang="ts">
 	import { Canvas } from '@threlte/core';
 	import PointCloudScene from './PointCloudScene.svelte';
+	import { Fullscreen, Maximize2, Minimize, Minimize2 } from '@lucide/svelte';
 
 	let { plyUrl }: { plyUrl: string } = $props();
+
+	let container: HTMLDivElement;
+	let enlarged = $state(false);
+	let isFullscreen = $state(false);
+
+	function toggleFullscreen() {
+		if (isFullscreen) {
+			document.exitFullscreen();
+		} else {
+			container.requestFullscreen();
+		}
+	}
 </script>
 
-<div class="aspect-video w-full overflow-hidden rounded-lg border border-surface-300-700">
+<div
+	bind:this={container}
+	onfullscreenchange={() => (isFullscreen = document.fullscreenElement === container)}
+	class="aspect-video overflow-hidden rounded-lg border border-surface-300-700 {enlarged
+		? 'relative left-1/2 w-screen -translate-x-1/2'
+		: 'w-full'}"
+>
+<div class="absolute flex space-x-2">
+	<button class="z-10 btn-icon preset-outlined-surface-300-700" onclick={toggleFullscreen}>
+		{#if isFullscreen}
+			<Minimize />
+		{:else}
+			<Fullscreen />
+		{/if}
+	</button>
+	{#if !isFullscreen}
+		<button class="z-10 btn-icon preset-outlined-surface-300-700" onclick={() => (enlarged = !enlarged)}>
+			{#if enlarged}
+				<Minimize2 />
+			{:else}
+				<Maximize2 />
+			{/if}
+		</button>
+	{/if}
+</div>
 	<Canvas>
 		<PointCloudScene {plyUrl} />
 	</Canvas>
